3e10bdc59d73182e349d5a7538e26be710da0b09
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-02-14 15:04:42 +0200

    initial commit

--- DOM/light-dark/script.js
+++ DOM/light-dark/script.js
@@ -1,8 +1,21 @@
-const header = document.querySelector("#header");
-const body = document.querySelector("body");
-const button = document.querySelector("#button");
+const paragraph = document.getElementById("headerParagraph");
+const body = document.getElementById("body");
+const button = document.getElementById("button");
 
-button.addEventListener("click", () =>{
-  header.sty
-  
-})
+let isLightMode = true; // This will track the state
+
+paragraph.innerHTML = "This is light mode";
+
+button.addEventListener("click", () => {
+    if (isLightMode) {
+        body.style.backgroundColor = "black";
+        paragraph.style.color = "white"
+        paragraph.innerHTML = "This is dark mode";
+    } else {
+        body.style.backgroundColor = "white";
+            paragraph.style.color = "black";
+        paragraph.innerHTML = "This is light mode";
+    }
+    isLightMode = !isLightMode; // Toggle the state
+});
+
