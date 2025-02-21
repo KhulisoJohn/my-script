bcadfd4e34c37f4b456b620b0bd76d3b97386969
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-02-21 09:42:37 +0200

    new change

--- DOM/light-dark/script.js
+++
@@ -1,8 +0,0 @@
-const header = document.querySelector("#header");
-const body = document.querySelector("body");
-const button = document.querySelector("#button");
-
-button.addEventListener("click", () =>{
-  header.sty
-  
-})
